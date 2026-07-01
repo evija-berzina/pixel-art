9c7c103912a2335906bd5f9905ae5cd25cb5aea4
eberzina <berzina3633@gmail.com>
2026-07-01 14:44:37 +0300

    Add Size component

---
+++ src/components/Size.tsx
@@ -0,0 +1,13 @@
+export function Size() {
+  return(
+    <>
+      <h2>Size</h2>
+      <div>
+        <button className="bg-mist-950 px-4 py-2 rounded-md border border-mist-800 cursor-pointer">8x8</button>
+        <button className="bg-mist-950 px-4 py-2 rounded-md border border-mist-800 cursor-pointer">16x16</button>
+        <button className="bg-mist-950 px-4 py-2 rounded-md border border-mist-800 cursor-pointer">32x32</button>
+        <button className="bg-mist-950 px-4 py-2 rounded-md border border-mist-800 cursor-pointer">64x64</button>
+      </div>
+    </>
+  )
+}
