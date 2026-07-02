4ed8025cb61679a46b85afd17c11e912df868f4e
eberzina <berzina3633@gmail.com>
2026-07-02 19:30:32 +0300

    Main app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import {Header} from './components/Header'
+import {Board} from './components/Board'
+import {Colors} from './components/Colors'
+import {Tools} from './components/Tools'
+import {Size} from './components/Size'
 import './App.css'
 
 function App() {
   
-
   return (
     <>
-      
+      <Header />
+      <main className='grid grid-cols-2 gap-4'>
+        <Board />
+        <aside>
+          <Colors />
+          <Tools />
+          <Size />
+        </aside>
+      </main>
     </>
   )
 }
